a7962a6941a2d8d774bca60669f7656e09a19f7b
shivashanmugam <sivashanmugam4444@gmail.com>
2025-09-16 09:20:17 +0530

    remove tools

--- tools/nodejs.py
+++
@@ -1,8 +0,0 @@
-from tools import Tools
-
-
-# Example of a concrete class inheriting from Tools
-class NodeJS(Tools):
-    def __init__(self, installation_command, verification_command, usage_instructions):
-        super().__init__(installation_command, verification_command, usage_instructions)
-
--- tools/tools.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC
-
-
-class Tools(ABC):
-
-    def __init__(self, installation_command, verification_command, usage_instructions):
-        self.installation_command = installation_command
-        self.verification_command = verification_command
-        self.usage_instructions = usage_instructions
