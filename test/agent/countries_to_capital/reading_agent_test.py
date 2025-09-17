208b405b946cff0adaf8c957cd6ffa3b97c9b61f
bala <kumaran.4353@gmail.com>
2025-09-17 15:43:44 +0000

    Add basic test for reading agent

---
+++ test/agent/countries_to_capital/reading_agent_test.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from pathlib import Path
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../")))
+
+from agent import Agent
+from agent_utils import AgentRunResult
+
+myAgent = Agent(
+    agent_type = "READING_AGENT",
+    model = "openai/mini-swe-agent-gpt5",
+    folder_to_mount = str(Path(__file__).parent / "countries_dir")
+)
+
+response : AgentRunResult = myAgent.run("Read the /countries_dir/countries.txt give me the capitals of each country.", timeout_in_seconds=300)
+
+print(
+    f"Status: {response.status}\n***Result:***\n{response.result}\n===\nError: {response.error}"
+)
