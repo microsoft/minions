0c94942ac834fd184deaa1cb65df5af1f7ebe461
shivashanmugam <sivashanmugam4444@gmail.com>
2025-09-22 11:13:54 +0530

    update version to 0.0.3 to publish to pip

--- src/microbots/__init__.py
+++ src/microbots/__init__.py
@@ -1,10 +1,5 @@
-from microbots.bot import ReadingBot, WritingBot, BrowsingBot, CustomBot
+from microbots.bot import BrowsingBot, CustomBot, ReadingBot, WritingBot
 
-__all__ = [
-    "ReadingBot",
-    "WritingBot",
-    "BrowsingBot",
-    "CustomBot"
-]
+__all__ = ["ReadingBot", "WritingBot", "BrowsingBot", "CustomBot"]
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
