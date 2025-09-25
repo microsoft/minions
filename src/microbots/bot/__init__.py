a7265b40322d7d3697cee0f8c4bd0b791d22182e
shivashanmugam <sivashanmugam4444@gmail.com>
2025-09-25 12:55:22 +0530

    exposing properly from library

--- src/microbots/bot/__init__.py
+++ src/microbots/bot/__init__.py
@@ -1,4 +1,7 @@
+from .BrowsingBot import BrowsingBot
+from .CustomBot import CustomBot
+from .LogAnalysisBot import LogAnalysisBot
 from .ReadingBot import ReadingBot
 from .WritingBot import WritingBot
 
-__all__ = ["ReadingBot", "WritingBot"]
+__all__ = ["BrowsingBot", "LogAnalysisBot", "ReadingBot", "WritingBot", "CustomBot"]
