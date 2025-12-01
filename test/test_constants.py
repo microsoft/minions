a5935917538c211f4f7405accd0ae9ba45efb832
bala <kumaran.4353@gmail.com>
2025-12-01 18:04:53 +0000

    Use a smaller model for testing in GitHub runner

--- test/test_constants.py
+++ test/test_constants.py
@@ -1,2 +1,4 @@
-LOCAL_MODEL_NAME = "qwen3-coder:latest"
+# LOCAL_MODEL_NAME = "qwen3-coder:latest"
+LOCAL_MODEL_NAME = "qwen3:latest"
+# LOCAL_MODEL_NAME = "qwen2.5-coder:latest"
 LOCAL_MODEL_PORT = "11434"
