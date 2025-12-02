e522335fc4f7fce69dcfedd6a70200d138b2c906
bala <kumaran.4353@gmail.com>
2025-12-02 10:35:37 +0000

    Try qwen2.5-coder

--- test/test_constants.py
+++ test/test_constants.py
@@ -1,6 +1,6 @@
 # LOCAL_MODEL_NAME = "qwen3-coder:latest"
 # LOCAL_MODEL_NAME = "qwen3:latest"
-# LOCAL_MODEL_NAME = "deepseek-r1:latest"
-# LOCAL_MODEL_NAME = "qwen2.5-coder:latest"
-LOCAL_MODEL_NAME = "mistral:latest"
+# LOCAL_MODEL_NAME = "deepseek-r1:latest" # fails and slow
+LOCAL_MODEL_NAME = "qwen2.5-coder:latest"
+# LOCAL_MODEL_NAME = "mistral:latest" # fails not responding in expected format
 LOCAL_MODEL_PORT = "11434"
